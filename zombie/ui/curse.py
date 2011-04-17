5c34e45848085c917f969dd45f6744a61d8fd3c1
termie <github@anarkystic.com>
2011-04-17 16:56:08 -0700

    a bit nicer

--- zombie/ui/curse.py
+++ zombie/ui/curse.py
@@ -9,6 +9,16 @@ import eventlet
 from zombie import shared
 from zombie.ui import base
 
+REALLY_BIG_STRING = """ASDBASDKL:AJSDLKAJSDAKLSJD
+ASDKLJASDLAJKSD
+ASD
+AKLSDJLAKSDJFASDFASDASDl
+ASDLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLASDASDASDLLLLLLLLLLLLLLLLLLLASDASDASDASD
+Asdasdlasdl
+asdafKLASDAKLSD
+
+ASDASDASDASD
+"""
 
 class CursesUi(base.Ui):
   def __init__(self, character, waddress):
@@ -27,22 +37,70 @@ class CursesUi(base.Ui):
     curses.noecho()
     #self.win.border(0, 0, 0, 0, 0, 0, 0, 0)
     self.max_y, self.max_x = self.win.getmaxyx()
-    self.win.hline(self.max_y - 2, 0, curses.ACS_HLINE, self.max_x)
     self.textwin = self.win.subwin(1, self.max_x, self.max_y - 1, 0)
     self.textbox = textpad.Textbox(self.textwin)
+    self.topbar = self.win.subwin(1, self.max_x, 0, 0)
+    self.main = self.win.subwin(self.max_y - 4, self.max_x, 2, 0)
+    self.main_y, self.main_x = self.main.getmaxyx()
+    self.draw_sections()
     self.win.refresh()
+    self.replace(REALLY_BIG_STRING)
+
+  def replace(self, text):
+    """Replace the main area with the text."""
+    self.main.clear()
+    self.main.addstr(0, 0, text)
+    self.main.refresh()
+
+  def append(self, text):
+    """Append text to the main area, scrolling it up."""
+    while text:
+      line = text[:self.main_x - 1]
+      text = text[self.main_x - 1:]
+      self._append_line(line)
+    self.main.refresh()
+
+  def _append_line(self, text):
+    self.main.move(0, 0)
+    self.main.deleteln()
+    self.main.move(self.main_y - 1, 0)
+    self.main.clrtoeol()
+    self.main.addnstr(self.main_y - 1, 0, text, self.main_x)
+
+
+  def draw_sections(self):
+    """Redraw the appropriate screen sections."""
+    self.win.hline(1, 0, curses.ACS_HLINE, self.max_x)
+    self.win.hline(self.max_y - 2, 0, curses.ACS_HLINE, self.max_x)
+    pass
+
+  def refresh(self):
+    """Refresh the screen."""
+    pass
 
   def input_loop(self):
     while True:
       data = self.textbox.edit()
-      self.handle_input(data)
-      self.win.move(2, 0)
-      self.win.deleteln()
-      self.win.insertln()
-      self.win.insstr(2, 0, data)
-      self.win.hline(self.max_y - 2, 0, curses.ACS_HLINE, self.max_x)
+      self.append(data)
       self.textwin.clear()
-      self.win.refresh()
+      self.textwin.refresh()
+      #self.handle_input(data)
+      #self.win.move(2, 0)
+      #self.win.deleteln()
+      #self.win.insertln()
+      #self.win.insstr(2, 0, data)
+      #self.win.move(0, 0)
+      #self.win.clrtobot()
+      #self.win.addstr(0, 0, REALLY_BIG_STRING)
+      #self.win.move(0, 0)
+      #self.win.deleteln()
+      #self.win.move(self.max_y - 3, 0)
+      #self.win.clrtoeol()
+      #self.win.addstr(self.max_y - 3, 0, "Hey everybody")
+
+      #self.win.hline(self.max_y - 2, 0, curses.ACS_HLINE, self.max_x)
+      #self.textwin.clear()
+      #self.win.refresh()
       #data = self.win.getch()
       #print self.textbox.edit()
       #self.textbox.do_command(data)
