f16138c3388543c286b14410d76f71015b46756f
termie <github@anarkystic.com>
2012-07-15 23:31:53 -0700

    remove a bunch of old files

--- run.sh
+++
@@ -1,12 +0,0 @@
-#!/bin/sh
-
-#workon zombie
-tmux new-session -s zombie -n shell -d
-tmux -v new-window -t zombie -n world -d
-tmux -v new-window -t zombie -n client -d
-
-tmux send-keys -t zombie:shell "workon zombie"
-tmux send-keys -t zombie:world "workon zombie"
-tmux send-keys -t zombie:world "python ./world_server.py"
-
-tmux attach
